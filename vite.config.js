c795cec7216fb8bbff7d220a12d28c4b704fc531
VivianOWork <vivianoverbeywork@gmail.com>
2025-07-09 20:11:39 -0400

    Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
-  base: "/VivianOWork/e-plantShopping",
+  base: "/github.com/VivianOWork/e-plantShopping",
   plugins: [react()],
 })
